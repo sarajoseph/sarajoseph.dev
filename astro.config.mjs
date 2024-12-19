401778abd43862743005252897514649fa21eac5
sarajoseph <sarajosephperez@gmail.com>
2024-12-19 20:29:15 +0100

    images optimization

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,5 @@
 import { defineConfig } from 'astro/config'
+import imagemin from 'vite-plugin-imagemin'
 export default defineConfig({
   i18n: {
     defaultLocale: 'es',
@@ -6,5 +7,38 @@ export default defineConfig({
     routing: {
       prefixDefaultLocale: false
     }
+  },
+  vite: {
+    plugins: [
+      imagemin({
+        gifsicle: {
+          optimizationLevel: 7,
+          interlaced: false,
+        },
+        optipng: {
+          optimizationLevel: 7,
+        },
+        mozjpeg: {
+          quality: 80,
+        },
+        svgo: {
+          plugins: [
+            {
+              name: 'removeViewBox',
+              active: false,
+            },
+            {
+              name: 'addAttributesToSVGElement',
+              params: {
+                attributes: [{ xmlns: 'http://www.w3.org/2000/svg' }],
+              },
+            },
+          ],
+        },
+        webp: {
+          quality: 85,
+        },
+      }),
+    ],
   }
 })
